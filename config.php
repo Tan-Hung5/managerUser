f339cc4cd332f35ec9bfdc0c51c7e48e74ef9a82
Phạm Tấn Hưng <122426826+Tan-Hung5@users.noreply.github.com>
2023-12-21 12:26:49 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ const _ACTION = 'dasboard';
 const _CODE = true; 
 
 define('_WEB_HOST','https://'.$_SERVER['HTTP_HOST']);
-define('_WEB_HOST_TEMPLE',_WEB_HOST.'/Project/manager_user/template');
+define('_WEB_HOST_TEMPLE',_WEB_HOST.'/template');
 
 define('_WEB_PATH',__DIR__);
 define('_WEB_PATH_TEMPLATE',_WEB_PATH.'/template');
